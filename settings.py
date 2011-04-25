02892d6b601eb7453527035ff4f6d274adb93264
Aaron Racicot <aaronr@z-pulley.com>
2011-04-24 20:20:18 -0700

    Updates to the conf files

--- settings.py
+++ settings.py
@@ -28,12 +28,16 @@ GOOGLE_ANALYTICS_CODE = ''
 GMAP_API_KEY = 'ABQIAAAAqm4SKuQITs2FoZu5Mz_ubhSAa4PBXeYnuhvAORHTrPcbaRaLShSQ_shhca1jNL9SBLkEG3k64ecDxw'
 ENABLE_GMAPS = False
 
-DATABASE_ENGINE = 'postgresql_psycopg2'          # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'cugos'        # Or path to database file if using sqlite3.
-DATABASE_USER = 'postgres'     # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cugos',
+        'USER': 'aaronr',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        },
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
