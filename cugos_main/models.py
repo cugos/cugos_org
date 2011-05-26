bc798d6a35ce0a7d26189552f44dbea80249e1a4
Ben Reilly <gallipoli@ubuntu.(none)>
2011-05-25 21:42:32 -0700

    we are not the roadflaws site

--- cugos_main/models.py
+++ cugos_main/models.py
@@ -19,24 +19,3 @@ class Event(models.Model):
     objects = models.GeoManager() # so we can use spatial queryset methods
     
     def __unicode__(self): return self.name
-
-
-class Flaws(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    popularity = models.IntegerField(blank=True,null=True)
-    severity = models.IntegerField(default=0 )
-    geometry = models.PointField(srid=4326)
-    tags = TagField()
-    objects = models.GeoManager() # so we can use spatial queryset methods
-    
-    def __unicode__(self): return self.name
-
-    class Meta:
-        verbose_name_plural = "Flaws" # otherwise django tries to make the Model name plural
-
-    def get_absolute_url(self):
-        self.geometry.transform(900913)
-        l = [str(x) for x in self.geometry.extent]
-        url = '/map/?extent=%s' % ','.join(l)
-        return url
